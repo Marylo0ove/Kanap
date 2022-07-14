78abbc8147f62cfa80786329d2789ff98a2c9d05
Marylo0ove <94191727+Marylo0ove@users.noreply.github.com>
2022-07-14 17:30:25 +0200

    test length

--- front/js/cart.js
+++ front/js/cart.js
@@ -28,5 +28,59 @@ for (let product of panier) {
                 </div>
               </article>`;
 });}
-    
+
+function getTotals(){
+
+  // Récupération du total des quantités
+  let elemsQtt = document.getElementsByName("itemQuantity");
+  console.log(elemsQtt);
+  let myLength = elemsQtt.length;
+  console.log(elemsQtt.length);
+  let totalQtt = 0;
+
+    for (let i = 0; i < elemsQtt.length; ++i) {
+        totalQtt += elemsQtt[i].valueAsNumber;
+    }
+
+    let productTotalQuantity = document.getElementById("totalQuantity");
+    productTotalQuantity.innerHTML = totalQtt;
+    console.log(totalQtt);
+
+  // Récupération du prix total
+  let totalPrice = 0;
+
+  for (var i = 0; i < myLength; ++i) {
+      totalPrice += (elemsQtt[i].valueAsNumber * panier[i].data.price);
+  }
+
+  let productTotalPrice = document.getElementById('totalPrice');
+  productTotalPrice.innerHTML = totalPrice;
+  console.log(totalPrice);
+}
+getTotals();
+
+//suppression
+function deleteProduct() {
+let btn_supprimer = document.getElementsByClassName("deleteItem");
+console.log(btn_supprimer);
+console.log(btn_supprimer.length);
+for (let j = 0; j < btn_supprimer.length; j++){
+  btn_supprimer[j].addEventListener("click" , (event) => {
+     // event.preventDefault();
+
+//Selection de l'element à supprimer en fonction de son id ET sa couleur
+let idDelete = panier[j].id;
+let colorDelete = panier[j].color;
+
+let index = panier.findIndex( el => el.id === idDelete && el.color === colorDelete );
+panier.splice(index,1);
+localStorage.setItem("panier", JSON.stringify(panier));
+
+//Alerte produit supprimé et refresh
+alert("Ce produit a bien été supprimé du panier");
+location.reload();
+  })
+}}   
+
+deleteProduct();
 //{return res.json();}
