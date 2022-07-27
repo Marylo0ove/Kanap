b6c3800f6bfa4223e71f5b70b926af3cc80b7c4e
Marylo0ove <94191727+Marylo0ove@users.noreply.github.com>
2022-07-27 11:01:11 +0200

    Panier createElement + form + alert panier vide
    Création du panier avec createElement plutôt que inner.HTML
    Pour le moment le click du bouton supprimer affiche une alerte "produit supprimé" et rafraichi la page, mais pas de MAJ dans le localStorage et le DOM
    Formulaire : le click du bouton commander est reconnu, les infos du formulaire son récupérées mais pas encore envoyé dans le localStorage.

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,59 +3,203 @@ let panier = JSON.parse(localStorage.getItem("products"));
 // S'il n'y a rien dans le localStorage : affichage d'un message d'erreur
 if (!panier){
   alert(`Votre panier est vide !`);
+  window.location.href = "index.html";
 }
 
 let totalQuantity = 0;
 let totalPrice = 0;
+
 for (let product of panier) {
   fetch(`http://localhost:3000/api/products/${product.id}`)
 .then((res) => res.json())
 .then((data) => {
-  document.getElementById("cart__items")
-    .innerHTML +=
-    `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
-                <div class="cart__item__img">
-                <img src="${data.imageUrl}" alt="${data.altTxt}">
-                </div>
-                <div class="cart__item__content">
-                  <div class="cart__item__content__description">
-                    <h2>${data.name}</h2>
-                    <p>${product.color}</p>
-                    <p>${data.price} €</p>
-                  </div>
-                  <div class="cart__item__content__settings">
-                    <div class="cart__item__content__settings__quantity">
-                      <p>Qté : </p>
-                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
-                    </div>
-                    <div class="cart__item__content__settings__delete">
-                      <p class="deleteItem">Supprimer</p>
-                    </div>
-                  </div>
-                </div>
-              </article>`;
+
+  let productArticle = document.createElement("article");
+    document.getElementById("cart__items").appendChild(productArticle);
+    productArticle.className = "cart__item";
+    productArticle.setAttribute('data-id', `${product.id}`);
+    productArticle.setAttribute('data-color', `${product.color}`);
+
+    // Insertion de l'élément "div"
+  let productDivImg = document.createElement("div");
+  productArticle.appendChild(productDivImg);
+  productDivImg.className = "cart__item__img";
+
+  // Insertion de l'image
+  let productImg = document.createElement("img");
+  productDivImg.appendChild(productImg);
+  productImg.src = `${data.imageUrl}`;
+  productImg.alt = `${data.altTxt}`;
+
+  // Insertion de l'élément "div"
+  let productItemContent = document.createElement("div");
+  productArticle.appendChild(productItemContent);
+  productItemContent.className = "cart__item__content";
+
+  // Insertion de l'élément "div"
+  let productItemContentDescription = document.createElement("div");
+  productItemContent.appendChild(productItemContentDescription);
+  productItemContentDescription.className = "cart__item__content__description";
+
+  // Insertion du titre h3
+  let productName = document.createElement("h2");
+  productItemContentDescription.appendChild(productName);
+  productName.textContent = `${data.name}`;
+
+  // Insertion de la couleur
+  let productColor = document.createElement("p");
+  productName.appendChild(productColor);
+  productColor.textContent = `${product.color}`;
+
+  // Insertion du prix
+  let productPrice = document.createElement("p");
+  productItemContentDescription.appendChild(productPrice);
+  productPrice.textContent = `${data.price} €`;
+
+  // Insertion de l'élément "div"
+  let productItemContentSettings = document.createElement("div");
+  productItemContent.appendChild(productItemContentSettings);
+  productItemContentSettings.className = "cart__item__content__settings";
+
+   // Insertion de l'élément "div"
+   let productItemContentSettingsQuantity = document.createElement("div");
+   productItemContentSettings.appendChild(productItemContentSettingsQuantity);
+   productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
+
+   // Insertion de "Qté : "
+   let productQte = document.createElement("p");
+   productItemContentSettingsQuantity.appendChild(productQte);
+   productQte.textContent = "Qté : ";
+
+   // Insertion de la quantité
+   let productQuantity = document.createElement("input");
+   productItemContentSettingsQuantity.appendChild(productQuantity);
+   productQuantity.setAttribute("type", "number");
+   productQuantity.className = "itemQuantity";
+   productQuantity.setAttribute("name", "itemQuantity");
+   productQuantity.setAttribute("min", "1");
+   productQuantity.setAttribute("max", "100");
+   productQuantity.value = `${product.quantity}`;
+   //productQuantity.addEventListener ('change');
+
+   // Insertion de l'élément "div"
+   let productItemContentSettingsDelete = document.createElement("div");
+   productItemContentSettings.appendChild(productItemContentSettingsDelete);
+   productItemContentSettingsDelete.className = "cart__item__content__settings__delete";
+
+   // Insertion de "p" supprimer
+   let productSupprimer = document.createElement("p");
+   productItemContentSettingsDelete.appendChild(productSupprimer);
+   productSupprimer.className = "deleteItem";
+   productSupprimer.textContent = "Supprimer";
+   productSupprimer.addEventListener('click', (event) => {
+    console.log('clicked');
+  event.preventDefault();
+  // enregistrer l'id et la couleur séléctionnés par le bouton supprimer
+  //let deleteId = product.id;
+  //let deleteColor = product.color;
+
+  // filtrer l'élément cliqué par le bouton supprimer
+  // en respectant les conditions du callback
+  let panierModif = panier.filter( elt => elt.id !== product.id || elt.color !== product.color);
+    
+  // envoyer les nouvelles données dans le localStorage
+  localStorage.setItem('product', JSON.stringify(panierModif));
+
+  // avertir de la suppression et recharger la page
+  alert('Votre article a bien été supprimé.');
+  window.location.href = "cart.html";
+  });
+
+  
 
   // Affichage du total de la quantité d'article
   let quantityNumber = parseInt(product.quantity);
   totalQuantity = parseInt (totalQuantity) + quantityNumber;
-  console.log(totalQuantity);
   document.getElementById("totalQuantity").textContent = totalQuantity;
   
   // Affichage du prix
   let priceNumber = parseInt(data.price * product.quantity);
   totalPrice = parseInt (totalPrice) + priceNumber;
-  console.log(totalPrice);
   document.getElementById("totalPrice").textContent = totalPrice;
 
 });}
 
+/*/ je supprime un produit dans le panier
+function deleteArticle() {
+  const deleteItem = document.getElementsByClassName("deleteItem");
+  console.log(deleteItem);
+  console.log(deleteItem.length);
 
+  for (let k of deleteItem) { 
+    
+    k.addEventListener('click', (event) => {
+      console.log('clicked');
+    event.preventDefault();
+      
+    // enregistrer l'id et la couleur séléctionnés par le bouton supprimer
+    let deleteId = product.id;
+    let deleteColor = product.color;
 
+    // filtrer l'élément cliqué par le bouton supprimer
+    // en respectant les conditions du callback
+    let panierModif = panier.filter( elt => elt.id !== deleteId || elt.color !== deleteColor);
+      
+    // envoyer les nouvelles données dans le localStorage
+    localStorage.setItem('product', JSON.stringify(panierModif));
 
+    // avertir de la suppression et recharger la page
+    alert('Votre article a bien été supprimé.');
+    window.location.href = "cart.html";
+    });
+  }
+}
+deleteArticle();
 
+/*function deleteItem() {
+  let buttons = document.getElementsByClassName("deleteItem");
+  console.log(buttons);
+  for (let button of Array.from(buttons)){
+      button.addEventListener("click", e => {
+        e.preventDefault();
+        console.log('clicked');
+          let canapeId = e.target.getAttribute("canapeId");
+          let canapeColor = e.target.getAttribute("canapeColor");
+          const searchDeleteItem = panier.find(element => element.id == canapeId && element.color == canapeColor);
+          panier = panier.filter(item => item != searchDeleteItem);
+          localStorage.setItem("Canape", JSON.stringify(panier));
+          window.location.href = "cart.html";
+      })
+  }
+}
+deleteItem();*/
 
+//Suppression
+/*let boutonsSupprimer = document.getElementsByClassName("deleteItem")
+console.log(boutonsSupprimer);
+console.log(boutonsSupprimer.length);
+for (let i = 0; i < boutonsSupprimer.length; i++ ) {
+  let button = boutonsSupprimer[i]
+  button.addEventListener ('click', function (event){
+    let buttonClicked = event.target
+    console.log('clicked')
+  })
+}*/
 
-function getForm() {
+/*function deleteItem(id, color) {
+  let panier = getCart();
+  for (i = 0; i < panier.length; i++) {
+    if (id === panier[i][0] && color === panier[i][1]) {
+      panier.splice(i, 1);
+      localStorage.setItem("panier", JSON.stringify(panier));
+      window.location.reload();
+    }
+  }
+}*/
+/* *****************************.  FORMULAIRE.   *********************************** */
+
+/**********************************.      GET ***********************************/ 
+//function getForm() {
 
     // Ajout des Regex
   let form = document.querySelector(".cart__order__form")
@@ -129,38 +273,42 @@ function getForm() {
       document.getElementById("emailErrorMsg").innerHTML = '';
     }
   };
+  //getForm();
+/* ______________________POST _____________________________*/
+
+function postForm() {
+  const order = document.getElementById("order");
+  console.log(order);
+  order.addEventListener('click', (event) => {
+    console.log('cliked');
+    event.preventDefault();
+
+  // je récupère les données du formulaire dans un objet
+  const contact = {
+    firstName : document.getElementById('firstName').value,
+    lastName : document.getElementById('lastName').value,
+    address : document.getElementById('address').value,
+    city : document.getElementById('city').value,
+    email : document.getElementById('email').value
+  }
+console.log(contact);
+})}
+function validControl() {
+  if (validFirstName(document.getElementById('firstName')) 
+  && validLastName(document.getElementById('lastName')) 
+  && validAddress(document.getElementById('address')) 
+  && validCity(document.getElementById('city')) 
+  && validEmail(document.getElementById('email'))) {
+    localStorage.setItem('contact', JSON.stringify(contact));
+    return true;
+  } 
 }
-getForm();
+validControl()
+  postForm();
 
 /*function getTotals(){
 
-  // Récupération du total des quantités
-  let elemsQtt = document.getElementsByName("itemQuantity");
-  console.log(elemsQtt);
-  let myLength = elemsQtt.length;
-  console.log(elemsQtt.length);
-  let totalQtt = 0;
-
-    for (let i = 0; i < elemsQtt.length; ++i) {
-        totalQtt += elemsQtt[i].valueAsNumber;
-    }
-
-    let productTotalQuantity = document.getElementById("totalQuantity");
-    productTotalQuantity.innerHTML = totalQtt;
-    console.log(totalQtt);
-
-  // Récupération du prix total
-  let totalPrice = 0;
-
-  for (var i = 0; i < myLength; ++i) {
-      totalPrice += (elemsQtt[i].valueAsNumber * panier[i].data.price);
-  }
-
-  let productTotalPrice = document.getElementById('totalPrice');
-  productTotalPrice.innerHTML = totalPrice;
-  console.log(totalPrice);
-}
-getTotals();
+  
 
 //suppression
 function deleteProduct() {
@@ -186,4 +334,27 @@ location.reload();
 }}   
 
 deleteProduct();*/
-//{return res.json();}
+
+/*document.getElementById("cart__items")
+    .innerHTML +=
+    `<article class="cart__item" data-id="${product.id}" data-color="${product.color}">
+                <div class="cart__item__img">
+                <img src="${data.imageUrl}" alt="${data.altTxt}">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>${data.name}</h2>
+                    <p>${product.color}</p>
+                    <p>${data.price} €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${product.quantity}">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`;*/
